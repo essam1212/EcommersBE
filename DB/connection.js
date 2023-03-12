980e46cdc94bb1222a98914e173b05a112ffe0b9
essam1212 <essam5ali2000@gmail.com>
2023-03-12 05:54:33 +0200

    v2

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 const connectDB  = async ()=>{
-    return await mongoose.connect(process.env.DBURI2)
-    .then(res=>console.log(`DB Connected successfully on .........${process.env.DBURI2} `))
+    return await mongoose.connect(process.env.DBURI)
+    .then(res=>console.log(`DB Connected successfully on .........${process.env.DBURI} `))
     .catch(err=>console.log(` Fail to connect  DB.........${err} `))
 }
 
